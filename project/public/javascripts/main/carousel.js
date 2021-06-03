4ab6d14bde925b1e365464e6ed2db0ef77fd29f5
국밥 <gosunjo123@gmail.com>
2021-06-03 08:09:39 +0000

    transition working

--- project/public/javascripts/main/carousel.js
+++ project/public/javascripts/main/carousel.js
@@ -1,4 +1,4 @@
-let items = document.querySelectorAll('#carousel-news .carousel-item')
+let items = document.querySelectorAll('#portfolio .news-item')
 
 items.forEach((el) => {
     // number of slides per carousel-item
@@ -13,4 +13,36 @@ items.forEach((el) => {
         el.appendChild(cloneChild.children[0])
         next = next.nextElementSibling
     }
+	if(!el.classList.contains('active')){
+		el.style.display = "none";
+	}
+})
+
+$(function(){
+	let firstElement = $('.news-item.active');
+	$('#newsCarouselLeft').click(function(){
+	});
+	
+	$('#newsCarouselRight').click(function(){
+		let current = $('.news-item.active');
+		let next = current.next();
+		if(!next) next = firstElement;
+		console.log(current, next);
+		next.css('translateX', '33.3%');
+		next.css('display', 'flex');
+		
+		next.css('transition', '2s all;');
+		current.css('transition', '2s all;');
+		
+		next.css('translateX', '0');
+		current.css('translateX', '-33.3%');
+		
+		next.css('transition', 'none');
+		current.css('transition', 'none');
+		
+		current.css('display', 'none');
+		
+		current.removeClass('active');
+		next.addClass('active');
+	});
 })
